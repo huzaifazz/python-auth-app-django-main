8edb1df10bcd793f2f6463e62d2802bb9639f642
huzaifazz <khuzaifaasad55@gmail.com>
2025-06-18 14:13:19 -0700

    urls updated

--- app/auth_app/urls.py
+++ app/auth_app/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import LoginView, SecureDataView, RefreshTokenView
+from .views import *
 
 urlpatterns = [
     path('login/', LoginView.as_view()),
     path('secure/', SecureDataView.as_view()),
     path('refresh/', RefreshTokenView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('logout-all/', LogoutAllDevicesView.as_view()),
 ]

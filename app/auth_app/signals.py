e110e48fd0b56673a527f99fbb787e932940ef65
huzaifazz <khuzaifaasad55@gmail.com>
2025-06-18 05:32:07 -0700

    add signals

--- app/auth_app/signals.py
+++ app/auth_app/signals.py
@@ -3,7 +3,9 @@ from django.dispatch import receiver
 import logging
 from django.db.models.signals import Signal
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger('auth_app')
+
+# user_logged_in = Signal()
 
 @receiver(user_logged_in)
 def log_login(sender, request, user, **kwargs):
@@ -16,3 +18,9 @@ refresh_token_used = Signal()
 @receiver(refresh_token_used)
 def log_refresh(sender, user, request, **kwargs):
     logger.info(f"{user.username} used refresh token from {request.META.get('REMOTE_ADDR')}")
+
+secure_data_token_used = Signal()
+
+@receiver(secure_data_token_used)
+def secure_data_view(sender, request, **kwargs):
+    logger.info(f"Secure data view accessed by {request.user.username} from {request.META.get('REMOTE_ADDR')}")

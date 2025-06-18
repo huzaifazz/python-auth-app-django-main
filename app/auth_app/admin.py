b1f883a4e178d71a5ce018f51b9bf58edd31bac3
huzaifazz <khuzaifaasad55@gmail.com>
2025-06-18 14:14:34 -0700

    register the model in admin

--- app/auth_app/admin.py
+++ app/auth_app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import BlacklistedToken
 
 # Register your models here.
+
+@admin.register(BlacklistedToken)
+class BlacklistedTokenAdmin(admin.ModelAdmin):
+    list_display = ['id', 'blacklisted_at']
+    search_fields = ['token']

7e53d02ca4cd04384eebbc8fe00016fb9e2fbd3c
huzaifazz <khuzaifaasad55@gmail.com>
2025-06-20 11:05:47 -0700

    add more routes

--- app/auth_app/urls.py
+++ app/auth_app/urls.py
@@ -7,4 +7,8 @@ urlpatterns = [
     path('refresh/', RefreshTokenView.as_view()),
     path('logout/', LogoutView.as_view()),
     path('logout-all/', LogoutAllDevicesView.as_view()),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('verify-email/', VerifyEmailView.as_view(), name='verify-email'),
+    path('request-otp/', RequestOTPView.as_view(), name='request-otp'),
+    path('verify-otp/', VerifyOTPView.as_view(), name='verify-otp'),
 ]

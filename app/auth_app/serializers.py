5f9818b2448ff785be560470562d16ce2a5319ae
huzaifazz <khuzaifaasad55@gmail.com>
2025-06-20 11:05:05 -0700

    add register serializer

--- app/auth_app/serializers.py
+++ app/auth_app/serializers.py
@@ -1,14 +1,22 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
 
-class UserSerializer(serializers.ModelSerializer):
+
+class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['username', 'password']
-        extra_kwargs = {'password': {'write_only': True}}
+        fields = ['username', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
     def create(self, validated_data):
-        user = User(username=validated_data['username'])
-        user.set_password(validated_data['password'])  # Hashing
+        user = User(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            is_active=False  # Not active until email confirmed
+        )
+        user.set_password(validated_data['password'])
         user.save()
         return user
+    
